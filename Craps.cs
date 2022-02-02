d786320715ec9ff44709ba9983fff166b63f70da
Roger <rogerandresgomezdavila@gmail.com>
2022-02-01 18:43:31 -0600

    Agregar archivos de proyecto.

---
+++ Craps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    internal class Craps
+    {
+
+        private int dado1;
+        private int dado2;
+
+
+        Craps ( int tirada1, int tirada2)
+        {
+            this.dado1 = tirada1;
+            this.dado2 = tirada2;
+        }
+
+        public int tirada()
+        {
+            int tirada = dado1 + dado2;
+
+            return tirada;
+            
+        }
+
+        public int getTirada1()
+        {
+            return dado1;
+        }
+        public void setTirada1(int tirada1)
+        {
+            this.dado1 = tirada1;
+        }
+        public int getTirada2()
+        {
+            return dado2;
+        }
+        public void setTirada2(int tirada2)
+        {
+            this.dado2 = tirada2;
+        }
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class btnTirar : Form
+    {
+
+        Random dado = new Random();
+        Boolean jugada = false;
+
+        
+
+
+
+        public btnTirar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTirar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+
+            int tira1 = dado.Next(1,7);
+            int tira2 = dado.Next(1,7);
+
+        }
+    }
+}
